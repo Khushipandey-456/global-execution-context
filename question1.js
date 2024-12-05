371fb5d9b74b06c512c8d55fdaa1ecb23ecdccf7
Khushipandey-456 <kp9761560@gmail.com>
2024-12-05 22:30:13 +0530

    ChangeAge

---
+++ question1.js
@@ -0,0 +1,15 @@
+var age=0; //crating a global variable
+// creating a displayAge function
+function displayAge() {
+  console.log("old age -"+age)//consoling old Age
+  }
+  //displayAge()
+  //creating a new function ChangeAge 
+function changeAge(newAge){
+    age=newAge;//updating old Age with newAge
+    console.log("NewAge - "+ age)
+}
+//calling displayAge function
+displayAge(25)
+//calling changeAge function
+changeAge(40)
